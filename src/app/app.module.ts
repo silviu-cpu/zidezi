77cd6b018f5f610543c9b88de4fa86f41c923731
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-07-10 22:55:19 +0300

    first

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'last-sogno',
+    appId: '1:602550191391:web:ac459f8542e3ee32b3dc2a',
+    storageBucket: 'last-sogno.appspot.com',
+    apiKey: 'AIzaSyBMdSjBTksUo4g14d88owZ3r3oXEqgzfXg',
+    authDomain: 'last-sogno.firebaseapp.com',
+    messagingSenderId: '602550191391',
+    measurementId: 'G-JXLKJVGZ6J',
+  }
+};
