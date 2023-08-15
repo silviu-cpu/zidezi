2d18fb8f5f54a94d8c530185ee0b8a48a7975800
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-08-15 23:31:07 +0300

    Fixed guard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,15 +5,16 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CartComponent } from './cart/cart.component';
 
-//import { AuthGuard } from './services/auth.guard';
+
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { AuthGuard } from './services/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full'},
   { path: 'home', component: HomeComponent},
   { path: 'login', component: LoginComponent},
   { path: 'cart', component: CartComponent},
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: DashboardComponent , canActivate: [AuthGuard] },
   { path: '**', component: HomeComponent}
 ];
 
--- src/app/services/auth.guard.ts
+++ src/app/services/auth.guard.ts
@@ -19,10 +19,6 @@ export class AuthGuard implements CanActivate {
         return new Promise((resolve, reject) => {
             this.afAuth.onAuthStateChanged((user) => {
                 if (user) {
-
-                    // if (!user.emailVerified)                            // if the user hasn't verified their email, send them to that page
-                    //     this.router.navigate(['/verify-email']);
-
                     resolve(true);
                 } else {
                     console.log('Auth Guard: user is not logged in');
