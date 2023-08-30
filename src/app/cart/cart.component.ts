382cb38293b563f0f322ded47edb698d16c46b66
Silviu Barbu <70367652+silviu-cpu@users.noreply.github.com>
2023-08-30 18:03:11 +0300

    Debug1

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -51,7 +51,7 @@ async onCheckout() {
     const stripe = await loadStripe('pk_test_51Nc6sEArMML4vzqfC2eBG4jCVWOpjs2Gub9QUV6XEM90DmLnAgZU8WCrdt6TPHrrYzBfzjnpDbeJsZ4lnItweppt003Cck5UN6');
     const { id } = response; // Assuming the API response has an 'id' field
     console.log("this is the response---")
-    console.log(response)
+    console.log(id)
 
     if (stripe) {
       await stripe.redirectToCheckout({
